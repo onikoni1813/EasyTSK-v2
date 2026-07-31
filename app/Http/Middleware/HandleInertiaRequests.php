5b9d66abf3d9c442c330956da103893fcb609bb6
Rashed Sarkar <onikoni1913@gmail.com>
2026-07-31 12:27:45 +0600

    Explicitly calculate version hash from build manifest to force Inertia client cache refresh

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -11,6 +11,9 @@ class HandleInertiaRequests extends Middleware
 
     public function version(Request $request): ?string
     {
+        if (file_exists($manifest = public_path('build/manifest.json'))) {
+            return md5_file($manifest);
+        }
         return parent::version($request);
     }
 
